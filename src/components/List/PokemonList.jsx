8ab7e428eaac09443353a0410b1f6bf15ba53d51
Your Name <you@example.com>
2025-08-19 01:45:44 +0300

    added details const in poke list

--- src/components/List/PokemonList.jsx
+++ src/components/List/PokemonList.jsx
@@ -1,18 +1,35 @@
 import { Link } from "react-router";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const PokemonList = ({ pokemon }) => {
+  const [list, setList] = useState([]);
+
+  const getPokemonList = async () => {
+    const details = await Promise.all(
+      pokemon.map(async (element) => {
+        const response = await axios.get(element.url);
+        return response.data;
+      })
+    );
+    setList(details);
+    console.log("details", details[0]);
+  };
+
+  useEffect(() => {
+    getPokemonList();
+  }, [pokemon]);
+
   return (
     <>
-      <h1>Pokemons Available</h1>
-      <ul>
+      <h1>Generation 1 Pokemon</h1>
+      <div>
         {pokemon.map((onePoke) => (
-          <li key={onePoke.name}>
-            <Link to={`/pokewiki/pokemons/${onePoke.name}`}>
-              {onePoke.name}
-            </Link>
-          </li>
+          <Link to={`/pokewiki/pokemons/${onePoke.name}`}>
+            <div key={onePoke.name}>{onePoke.name}</div>
+          </Link>
         ))}
-      </ul>
+      </div>
     </>
   );
 };
