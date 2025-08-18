674bf75078f5de96433b4ff99bc828a2cec20f7d
Ali Buali <alibuali3660@gmail.com>
2025-08-18 12:15:50 +0300

    Api connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router";
+import axios from "axios";
 
 import PokemonList from "./components/List/PokemonList";
 import NavBar from "./components/NavBar/NavBar";
 
 const App = () => {
+  const [pokemon, setPokemon] = useState([]);
+  const getPokemon = async () => {
+    const response = await axios.get("https://pokeapi.co/api/v2/pokemon/");
+    console.log(response.data);
+    setPokemon(response.data);
+  };
+
+  useEffect(() => {
+    getPokemon();
+  }, []);
+
   return (
     <Router>
       <NavBar />
@@ -13,6 +25,7 @@ const App = () => {
           path="/pokewiki/pokemons"
           element={<PokemonList pokemon={pokemon} />}
         />
+        <Route path="/pokewiki/pokemons/:" />
       </Routes>
     </Router>
   );
