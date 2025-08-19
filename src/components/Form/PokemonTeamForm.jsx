121acd0de2cde13687e8c9bfcbe0dfb78030ffc7
unknown <mohammed98ali.ma@gmail.com>
2025-08-19 13:31:52 +0300

    commit before merging

--- src/components/Form/PokemonTeamForm.jsx
+++ src/components/Form/PokemonTeamForm.jsx
@@ -1,15 +1,31 @@
 import { useState } from "react"
 import { useNavigate } from "react-router"
 
+
 const PokemonTeamForm = ({pokemon}) => {
+
+const [isSubmitting, setIsSubmitting] = useState(false)
+
+
+
 // form Data: 
-const [pokeTeam, setPokeTeam] = useState([])
+const [pokeTeam, setPokeTeam] = useState({
+pokemon1:'',
+pokemon2:'',
+pokemon3:'',
+pokemon4:'',
+pokemon5:'',
+pokemon6:''
+
+})
 
 
-const handleSubmit = (event) => {
-
+const handleSubmit = async (event) => {
     setPokeTeam({...pokeTeam, [event.target.name]: event.target.value})
+    const response = await create(pokeTeam)
+
 }
+
 return(
     <>
     <h1>Make a Pokemon Team</h1>
