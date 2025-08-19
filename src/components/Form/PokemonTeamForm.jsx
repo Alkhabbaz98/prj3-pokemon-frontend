16e57d1ab3edcab4b33aed229dd818b3a9f01d3b
unknown <mohammed98ali.ma@gmail.com>
2025-08-19 12:43:56 +0300

    added select and option tags for the form component

--- src/components/Form/PokemonTeamForm.jsx
+++ src/components/Form/PokemonTeamForm.jsx
@@ -2,15 +2,20 @@ import { useState } from "react"
 import { useNavigate } from "react-router"
 
 const PokemonTeamForm = ({pokemon}) => {
-
+// form Data: 
 const [pokeTeam, setPokeTeam] = useState([])
 
+
+const handleSubmit = (event) => {
+
+    setPokeTeam({...pokeTeam, [event.target.name]: event.target.value})
+}
 return(
     <>
     <h1>Make a Pokemon Team</h1>
     <p>Pokemons Available</p>
-    <form>
-
+    <form >
+    <label htmlFor="name">Pokemon 1</label>
     <select
         name="pokemon"
         id="pokemon"
@@ -20,6 +25,7 @@ return(
         <option multiple value={onePoke.name}>{onePoke.name}</option>
     ))}
     </select>
+    <label htmlFor="name">Pokemon 2</label>
     <select
         name="pokemon"
         id="pokemon"
@@ -30,16 +36,7 @@ return(
     ))}
     </select>
     <br />
-    <select
-        name="pokemon"
-        id="pokemon"
-    >
-
-    {pokemon.map((onePoke) => (
-        <option multiple value={onePoke.name}>{onePoke.name}</option>
-    ))}
-    </select>
-    <br />
+    <label htmlFor="name">Pokemon 3</label>
     <select
         name="pokemon"
         id="pokemon"
@@ -51,7 +48,8 @@ return(
     </select>
     <br />
     
-        <select
+    <label htmlFor="name">Pokemon 4</label>
+    <select
         name="pokemon"
         id="pokemon"
     >
@@ -61,7 +59,9 @@ return(
     ))}
     </select>
     <br />
-        <select
+
+    <label htmlFor="name">Pokemon 5</label>
+    <select
         name="pokemon"
         id="pokemon"
     >
@@ -70,8 +70,20 @@ return(
         <option multiple value={onePoke.name}>{onePoke.name}</option>
     ))}
     </select>
-    
+    <br />
+    <label htmlFor="name">Pokemon 6</label>
+    <select
+        name="pokemon"
+        id="pokemon"
+    >
+
+    {pokemon.map((onePoke) => (
+        <option multiple value={onePoke.name}>{onePoke.name}</option>
+    ))}
+    </select>
+
       
+    <button onClick={handleSubmit}> Submit your team </button>
       </form>
     </>
 )
