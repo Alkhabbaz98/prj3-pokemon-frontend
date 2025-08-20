104edb97794734ccbbb85e3eca9aa76de6d54a14
Your Name <you@example.com>
2025-08-20 20:03:54 +0300

    added to my pokemon team page

--- src/components/Team/PokemonTeamHomepage.jsx
+++ src/components/Team/PokemonTeamHomepage.jsx
@@ -11,7 +11,7 @@ const PokemonTeamHomepage = ({}) => {
     const getTeam = await showTeam();
     const token = localStorage.getItem("token");
     const decoded = jwtDecode(token);
-    const userTeams = getTeam.filter((team) => team.user === decoded.id);
+    const userTeams = getTeam.filter((team) => team.user._id === decoded.id);
     setTeams(userTeams);
   };
 
@@ -29,21 +29,27 @@ const PokemonTeamHomepage = ({}) => {
   return (
     <>
       {teams.length ? (
-        teams.map((element) => {
-          return (
-            <>
-              <p>{element.pokemon1}</p>
-              <p>{element.pokemon2}</p>
-              <p>{element.pokemon3}</p>
-              <p>{element.pokemon4}</p>
-              <p>{element.pokemon5}</p>
-              <p>{element.pokemon6}</p>
-              <button>Edit</button>
-              <button onClick={() => handleDelete(element._id)}>Delete</button>
-              <br />
-            </>
-          );
-        })
+        <>
+          <h1>{teams[0].user?.username}'s Pokemon Teams</h1>
+          {teams.map((element, index) => {
+            return (
+              <div key={element._id}>
+                <h1>Teams {index + 1}:</h1>
+                <p>{element.pokemon1}</p>
+                <p>{element.pokemon2}</p>
+                <p>{element.pokemon3}</p>
+                <p>{element.pokemon4}</p>
+                <p>{element.pokemon5}</p>
+                <p>{element.pokemon6}</p>
+                <button>Edit</button>
+                <button onClick={() => handleDelete(element._id)}>
+                  Delete
+                </button>
+                <br />
+              </div>
+            );
+          })}
+        </>
       ) : (
         <h2>You don't have any teams yet.</h2>
       )}
