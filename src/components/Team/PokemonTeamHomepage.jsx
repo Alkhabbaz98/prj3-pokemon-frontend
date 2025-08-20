057bb1bc9e413b8607e0353a42846251ddb1437c
unknown <mohammed98ali.ma@gmail.com>
2025-08-20 15:23:12 +0300

    3.23pm

--- src/components/Team/PokemonTeamHomepage.jsx
+++ src/components/Team/PokemonTeamHomepage.jsx
@@ -1,5 +1,15 @@
+import axios from "axios";
+import { useState } from "react";
 import { Link } from "react-router";
+
+
 const PokemonTeamHomepage = () => {
+  const baseUrl = import.meta.env.VITE_BACK_END_SERVER_URL
+  const [test, setTest] = useState([])
+  const testFun = async () => {
+    await axios.showTeam(`${baseUrl}/pokewiki/new`)
+  }
+  
   return (
     <div>
       <Link to="myteam">
